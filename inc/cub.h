e099b898b94f48797019033a2ccc38b9382cfcd3
Marianna Zhukova <mzhukova@c1r1s3.42wolfsburg.de>
2024-09-25 16:25:25 +0200

    function model

--- inc/cub.h
+++ inc/cub.h
@@ -6,8 +6,13 @@
 /*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 10:12:38 by mzhukova          #+#    #+#             */
-/*   Updated: 2024/09/25 10:13:01 by mzhukova         ###   ########.fr       */
+/*   Updated: 2024/09/25 16:16:47 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
+
+void create_Frame(int *Coords, ...);  //Cords=[x][y][z][x'][y'][z']
+void p_movement(char *Coords, void *key_pressed);
+void m_movement(char *Coords, void *mouse_moved);
+
