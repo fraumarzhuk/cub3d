d90495c2ae0473863f526dd591e9067c932c2d82
Tobias Laukat <tlaukat@c2r8s7.42wolfsburg.de>
2024-10-10 20:34:52 +0200

    prep for home

---
+++ src/Tobi/main.c
@@ -0,0 +1,10 @@
+#include "../../inc/cub.h"
+#include "../Math-functions/xMath.h"
+
+int get_pixel_color(t_data *data, int*pos, int *dir);
+
+int main(int argc, char **argv)
+{
+
+    printf("%d\n", get_pixel_color(NULL, NULL, NULL));
+}
